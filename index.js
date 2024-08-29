3ac53b9149fbe5021dd7b07deff8789daddcc25c
kimberli18 <127845679+kimberli18@users.noreply.github.com>
2024-08-29 01:06:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let anoAtual = 2024;
+const nomeCompleto = "Kimberli Evelin de Assis Vaz";
+let idade = 19;
+const cursoFaculdade = "Sistemas para Internet";
+let cidadeAtual = "Curitiba"; 
+
+console.log(`Olá eu sou a ${nomeCompleto}, tenho ${idade} anos, estou cursando`);
+
+anoAtual = 2025;
+
+console.log(anoAtual);
+
